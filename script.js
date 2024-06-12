db50bb2b8b1fb8bd01dce16485b03c6928466be7
kevin.corithiano.projeto <161588515+kevinribeirodepqulq@users.noreply.github.com>
2024-06-12 08:54:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var meuFormulario = document.getElementById("formulario");
+meuFormulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  document.getElementById("formulario")
+
+//;Verificar se a diferença entre dois números é positiva.
+
+
+//Verificar se a diferença entre dois números é positiva.
+
+
+
+  //Captura o que está dentro do primeiro input no formulário
+  /Number - Transforma o texto capturado em Numero
+  let primeiro_numero_ = Number(document.getElementById("primeiro_numero_").value
+
+  //Captura o que está dentro do segundo input no formulário
+  |Number - Transforma o texto capturado em Numero
+  /let segundo_numero_ = Number(document.getElementById("segundo_numero_").value
+
+  Realiza a soma do valor que está na variável "primeiro_numero_" com o valor que está na
+  variável "segundo_numero_"
+  E a variável "resultado_" recebe o valor da s
+  let resultado_soma = primeiro_numero_ + segundo_numero_;
+
+  Aqui vai escrever no input "resultado_" o valor que está armazenado na variável "resultado_"
+  conssole.log(ressultado)
+
+
+}
